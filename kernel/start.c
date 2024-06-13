5488e3017b9b9360b6850198e83892a2127ba1f7
Emilien Tellier <emilien.tellier@grenoble-inp.org>
2024-06-13 09:35:00 +0200

    FIX: Changing stack management before syscall IT.
    - Adding kernel start.c (corrected with proper init it)
    - Adding console_putbytes syscall

--- kernel/start.c
+++ kernel/start.c
@@ -8,67 +8,6 @@
 #include "string.h"
 #include "mem.h"
 
-
-void proc12() {
-	sleep(2);
-	printf("exit child\n");
-	exit(4);
-}
-
-void proc13() {
-	sleep(2);
-	printf("exit child\n");
-	exit(99);
-}
-
-void proc1() {
-	cli();
-	sti();
-	int retval;
-	char name[MAX_PROC_NAME_SIZE];
-	int32_t cpid;
-	printf("proc1 priority is %d\n", get_process(getpid())->priority);
-	chprio(getpid(),256);
-	printf("proc1 priority is %d\n", get_process(getpid())->priority);
-	for (int i = 0; i < 3 ; i++) {
-		sprintf(name, "proc_%d", i+10);
-		cpid = start((void*)proc12, 256, 3, name, 0);
-		cli();
-		printf("created process pid=%d, parent_pid=%d\n", cpid, process_table->table[cpid]->ppid);
-		sti();
-	}
-	cpid = start((void*)proc13, 256, 3, "proc_999", 0);
-	cli();
-	printf("created process pid=%d, parent_pid=%d\n", cpid, process_table->table[cpid]->ppid);
-	sti();
-	waitpid(cpid, &retval);
-	printf("child (pid %d) returned : %d\n", cpid, retval);
-}
-
-void proc2() {
-	cli();
-	sti();
-	int retval;
-	char name[MAX_PROC_NAME_SIZE];
-	int32_t cpid;
-	printf("proc2 priority is %d\n", get_process(getpid())->priority);
-	chprio(getpid(),1);
-	printf("proc2 priority is %d\n", get_process(getpid())->priority);
-	for (int i = 0; i < 3 ; i++) {
-		sprintf(name, "proc_%d", i+10);
-		cpid = start((void*)proc12, 256, 3, name, 0);
-		cli();
-		printf("created process pid=%d, parent_pid=%d\n", cpid, process_table->table[cpid]->ppid);
-		sti();
-	}
-	cpid = start((void*)proc13, 256, 3, "proc_999", 0);
-	cli();
-	printf("created process pid=%d, parent_pid=%d\n", cpid, process_table->table[cpid]->ppid);
-	sti();
-	waitpid(-1, &retval);
-	printf("child (pid %d) returned : %d\n", cpid, retval);
-}
-
 void idle(void){
 	while(1){
 		sti();
@@ -77,18 +16,21 @@ void idle(void){
 	}
 }
 
-void* test_it49(int sn, char* arg2){
+void* test_it49(int sn, int arg2, int arg3){
 	if(sn == 0){
 		printf("SOFTWARE INTERUPTED WITH PARAM 1 %d\n",sn);
 		printf("GETPID RETURNS %d\n", getpid());
 		return((void*)getpid());
 	} else if (sn == 1) {
-		printf("%s",arg2);
+		printf("SOFTWARE INTERUPTED WITH PARAM 1 %d\n",sn);		
+		printf("SOFTWARE INTERUPTED WITH PARAM 2 %s\n",(char*)arg2);
+		console_putbytes((char *)arg2,arg3);
 		return 0;
-	} else {
+	} else if (sn == 2){
 		printf("SOFTWARE INTERUPTED WITH PARAM 1 %d\n",sn);
+		printf("SOFTWARE INTERUPTED WITH PARAM 2 %d\n",arg2);
 		return 0;
-	}
+	} else {return 0;}
 }
 
 void kernel_start(void)
@@ -100,6 +42,7 @@ void kernel_start(void)
 
 	init_clock();
 	init_IT_handlers(32, IT_32_handler);
+	init_IT_handlers(49, IT_49_handler);
 	mask_IRQ(0, false);
 	process_table = init_process_table();
 
--- user/syscall.h
+++ user/syscall.h
@@ -2,6 +2,7 @@
 #define __SYSCALL_H__
 
 int getpid(void);
-int getprio(void);
+int getprio(int pid);
+void console_putbytes(const char *s, int len);
 
 #endif
