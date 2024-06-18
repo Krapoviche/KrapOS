03642ddd6866a6b6a7c98968bb0a520822e9b1a9
Emilien Tellier <emilien.tellier@grenoble-inp.org>
2024-06-18 15:37:20 +0200

    TEST: Adding back test.c and test.h to run tests in shell

---
+++ user/test.h
@@ -0,0 +1,33 @@
+#ifndef __TEST_H__
+#define __TEST_H__
+
+#include "stdio.h"
+#include "string.h"
+
+void test1(void);
+void test2(void);
+void test3(void);
+void test4(void);
+void test5(void);
+void test6(void);
+void test7(void);
+void test8(void);
+void test9(void);
+void test10(void);
+void test11(void);
+void test12(void);
+void test13(void);
+void test14(void);
+void test15(void);
+void test16(void);
+void test17(void);
+void test18(void);
+void test19(void);
+void test20(void);
+
+unsigned char 
+inb(unsigned short port);
+void 
+outb(unsigned char value, unsigned short port);
+
+#endif
