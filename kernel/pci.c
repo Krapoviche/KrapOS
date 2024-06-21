bfcf8af6f1724c09fa75f19df5df183ee1a60b3f
Emilien Tellier <emilien.tellier@grenoble-inp.org>
2024-06-21 11:09:38 +0200

    FEATURE: Adding BIOS32 interractions to read PCI Bios info

--- kernel/pci.c
+++ kernel/pci.c
@@ -1,4 +1,17 @@
 #include "pci.h"
+
+static struct {
+	unsigned long address;
+	unsigned short segment;
+} bios32_indirect = { 0, KERNEL_CS };
+
+
+static struct {
+	unsigned long address;
+	unsigned short segment;
+} pci_indirect = { 0, KERNEL_CS };
+
+
 int pci_read_config_byte(unsigned char bus, unsigned char device_fn, unsigned char where, unsigned char *value)
 {
     outl(CONFIG_CMD(bus,device_fn,where), 0xCF8);
@@ -63,6 +76,16 @@ void lspci(){
     char* vendor_name = mem_alloc(sizeof(char) * 50);
     char* device_name = mem_alloc(sizeof(char) * 50);
     unsigned short vendor_id, device_id;
+
+    uint32_t* service_dir_header = find_bios32_service_dir_header();
+    bios32* bios32_info =  mem_alloc(sizeof(bios32));
+    read_bios32(service_dir_header, bios32_info);
+    uint32_t* pci_service = read_bios32_service(PCI_SERVICE, bios32_info);
+    pcibios* pcibios_info =  mem_alloc(sizeof(pcibios));
+    read_pcibios(pci_service, pcibios_info);
+
+    printf("PCI BIOS VERSION %d.%d\n", pcibios_info->major_revision, pcibios_info->minor_revision);
+
     for(uint32_t bus = 0; bus < 256; bus++)
     {
         for(uint32_t slot = 0; slot < 256; slot++)
@@ -124,4 +147,77 @@ void lspci(){
     mem_free(value8, sizeof(unsigned char));
     mem_free(vendor_name, sizeof(char) * 50);
     mem_free(device_name, sizeof(char) * 50);
+}
+
+uint32_t * find_bios32_service_dir_header(){
+    uint32_t* start_address = (uint32_t*) 0xE0000;
+    uint32_t* end_address = (uint32_t*) 0xFFFFF;
+
+    for( ; start_address < end_address ;  start_address++){
+        if(*start_address == 0x5F32335F){
+            return start_address;
+        }
+    }
+    return (uint32_t*)0xFFFF;
+}
+
+void read_bios32(uint32_t* start_address, bios32* bios32_info){
+    bios32_info->signature = *start_address;
+    bios32_info->address = *(start_address + 1);
+
+    uint8_t* tmp = (uint8_t*)(start_address + 2); // Next 3 fields are 1 byte long
+
+    bios32_info->revision = *tmp;
+    bios32_info->length = *(tmp + 1);
+    bios32_info->checksum = *(tmp + 2);
+
+    bios32_info->reserved[0] = *(tmp + 3); // First byte of reserved
+    bios32_info->reserved[1] = *(start_address + 3); // Last 4 bytes of reserved (start address + 3 == start address + 2 + 1 byte * 4)
+}
+
+uint32_t* read_bios32_service(uint32_t service, bios32* bios32_info){
+    bios32_indirect.address = bios32_info->address;
+
+	unsigned char return_code;	/* %al */
+	unsigned long address;		/* %ebx */
+	unsigned long length;		/* %ecx */
+	unsigned long entry;		/* %edx */
+
+	__asm__("lcall (%%edi)"
+		: "=a" (return_code),
+		  "=b" (address),
+		  "=c" (length),
+		  "=d" (entry)
+		: "0" (service),
+		  "1" (0),
+		  "D" (&bios32_indirect));
+
+    if(return_code != 0x80 && return_code != 0x81)
+        return  (uint32_t*)(address + entry);
+    return NULL;
+}
+
+uint32_t read_pcibios(uint32_t* pcibios_entry, pcibios* pcibios_info){
+    pci_indirect.address = (uint32_t)pcibios_entry;
+
+    uint32_t pack;
+
+
+    __asm__("lcall (%%edi)\n\t"
+        "jc 1f\n\t"
+        "xor %%ah, %%ah\n"
+        "1:\tshl $8, %%eax\n\t"
+        "movw %%bx, %%ax"
+        : "=d" (pcibios_info->signature),
+            "=a" (pack)
+        : "1" (PCIBIOS_PCI_BIOS_PRESENT),
+            "D" (&pci_indirect)
+        : "bx", "cx");
+
+
+    pcibios_info->present_status = (pack >> 16) & 0xff;
+    pcibios_info->major_revision = (pack >> 8) & 0xff;
+    pcibios_info->minor_revision = pack & 0xff;
+
+    return 0;
 }
--- kernel/pci.h
+++ kernel/pci.h
@@ -7,9 +7,38 @@
 #include "segment.h"
 #include "mem.h"
 #include "string.h"
+
+#define PCI_SERVICE		(('$' << 0) + ('P' << 8) + ('C' << 16) + ('I' << 24))
+#define PCI_SIGNATURE		(('P' << 0) + ('C' << 8) + ('I' << 16) + (' ' << 24))
+#define PCIBIOS_PCI_BIOS_PRESENT 	0xb101
+
 #define CONFIG_CMD(bus, device_fn, where)   (0x80000000 | (bus << 16) | (device_fn << 8) | (where & ~3))
+
+typedef struct bios32{
+    uint32_t address;
+    uint32_t signature;
+    uint8_t revision;
+    uint8_t length;
+    uint8_t checksum;
+    uint8_t reserved[5];
+} bios32;
+
+typedef struct pcibios{
+	uint32_t signature;
+	uint8_t present_status;
+	uint8_t major_revision;
+	uint8_t minor_revision;
+} pcibios;
+
 void lspci();
+
 int pci_read_config_byte(unsigned char bus, unsigned char device_fn, unsigned char where, unsigned char *value);
 int pci_read_config_word (unsigned char bus, unsigned char device_fn, unsigned char where, unsigned short *value);
 int pci_read_config_dword (unsigned char bus, unsigned char device_fn, unsigned char where, unsigned int *value);
+
+uint32_t * find_bios32_service_dir_header();
+void read_bios32(uint32_t* start_address, bios32* bios32_info);
+uint32_t* read_bios32_service(uint32_t service, bios32* bios32_info);
+uint32_t read_pcibios(uint32_t* pcibios_entry, pcibios* pcibios_info);
+
 #endif
